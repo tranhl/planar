73bd916c5cf0e64cb65e20c27f975a924764f239
Long Hoang Tran <tranhl@gmx.com>
2017-11-13 22:35:02 +1100

    chore: added test cases for Planar's constructor

---
+++ test/constructor.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const chai = require('chai');
+const mocha = require('mocha');
+const Planar = require('../index');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should;
+
+describe('Planar(grid)', function () {
+    let numberGrid = undefined;
+    let stringGrid = undefined;
+    let objectGrid = undefined;
+    let mixedGrid = undefined;
+
+    before(function () {
+        numberGrid = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
+        stringGrid = [
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9']
+        ];
+        objectGrid = [
+            [{ value: 1 }, { value: 2 }, { value: 3 }],
+            [{ value: '4' }, { value: '5' }, { value: '6' }],
+            [{ value: 7 }, { value: 8 }, { value: 9 }]
+        ];
+        mixedGrid = [
+            [1, '2', { value: 3 }],
+            [4, '5', { value: 6 }],
+            [7, '8', { value: 9 }]
+        ]
+    });
+
+    it('should return an Error object if the passed grid is malformed', function () {
+        assert.instanceOf(new Planar(numberGrid), Planar);
+        assert.instanceOf(new Planar(stringGrid), Planar);
+        assert.instanceOf(new Planar(objectGrid), Planar);
+        assert.instanceOf(new Planar(mixedGrid), Planar);
+        assert.instanceOf(new Planar(1), Error);
+        assert.instanceOf(new Planar({}), Error);
+        assert.instanceOf(new Planar([1, 2, 3]), Error);
+    });
+
+    it('should instantiate without modifying the passed grid', function() {
+        assert.deepEqual(new Planar(numberGrid).grid, numberGrid);
+        assert.deepEqual(new Planar(stringGrid).grid, stringGrid);
+        assert.deepEqual(new Planar(objectGrid).grid, objectGrid);
+        assert.deepEqual(new Planar(mixedGrid).grid, mixedGrid);
+    });
+
+    it('should instantiate with the correct width and height', function() {
+        let planar = new Planar(numberGrid);
+        assert.strictEqual(planar.width, 3);
+        assert.strictEqual(planar.height, 3);
+    });
+});
