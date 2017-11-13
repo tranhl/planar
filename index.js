e051b0dc7276ade2d8bdd27d4456e2761198f4cc
Long Hoang Tran <tranhl@gmx.com>
2017-11-13 21:50:46 +1100

    feat: implemented Planar.get

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ let Planar = function (grid) {
     this.grid = grid;
 };
 
+/** 
+ * Get the value of a cell.
+ * 
+ * @param {any} row
+ * @param {any} col
+ * @returns The value of the cell.
+ * @throws 
+ */
+Planar.prototype.get = function (row, col) {
+    try {
+        return this.grid[row][col];
+    } catch (err) {
+        return new Error(`Out of bounds planar access in row ${row} col ${col}.`);
+    }
+}
+
 module.exports = Planar;
