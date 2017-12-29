045ad908a803fc355a8d8df0254c27b57d9f7e9b
Long Hoang Tran <tranhl@gmx.com>
2017-12-29 21:46:23 +1100

    chore: added Planar.fillArea test cases

--- test/fill.js
+++ test/fill.js
@@ -7,7 +7,7 @@ const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should;
 
-describe('Planar.fill', function () {
+describe('Planar.fill(value)', function () {
     let grid = undefined;
 
     before(function () {
@@ -28,5 +28,57 @@ describe('Planar.fill', function () {
 
     it('should return an error object if no argument is provided', function () {
         assert.instanceOf(new Planar(grid).fill(), Error);
+    });
+});
+
+describe('Planar.fillArea(value, row, col, height, width)', function () {
+    let grid = undefined;
+
+    beforeEach(function () {
+        grid = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ];
+    });
+
+    it('should fill the specified grid cells with the provided value', function () {
+        assert.deepEqual(new Planar(grid).fillArea('x', 0, 0, 2, 2).grid(), [
+             ['x', 'x', 3],
+             ['x', 'x', 6],
+             [7, 8, 9]
+        ]);
+
+        assert.deepEqual(new Planar(grid).fillArea('x', 0, 1, 2, 2).grid(), [
+            [1, 'x', 'x'],
+            [4, 'x', 'x'],
+            [7, 8, 9]
+        ]);
+
+        assert.deepEqual(new Planar(grid).fillArea('x', 1, 0, 2, 2).grid(), [
+            [1, 2, 3],
+            ['x', 'x', 6],
+            ['x', 'x', 9]
+        ]);
+
+        assert.deepEqual(new Planar(grid).fillArea('x', 1, 1, 2, 2).grid(), [
+            [1, 2, 3],
+            [4, 'x', 'x'],
+            [7, 'x', 'x']
+        ]);
+    });
+
+    it('should return an error object if the specified area is out of bounds', function () {
+        assert.instanceOf(new Planar(grid).fillArea('x', -1, -1, 2, 2), Error);
+        assert.instanceOf(new Planar(grid).fillArea('x', 0, 1, 3, 3), Error);
+        assert.instanceOf(new Planar(grid).fillArea('x', 1, 0, 3, 3), Error);
+        assert.instanceOf(new Planar(grid).fillArea('x', 0, 0, 4, 4), Error);
+    })
+
+    it('should return an error object if the incorrect number of arguments is provided', function () {
+        assert.instanceOf(new Planar(grid).fillArea('x'), Error);
+        assert.instanceOf(new Planar(grid).fillArea('x', 0), Error);
+        assert.instanceOf(new Planar(grid).fillArea('x', 0, 0), Error);
+        assert.instanceOf(new Planar(grid).fillArea('x', 0, 0, 0), Error);
     });
 });
